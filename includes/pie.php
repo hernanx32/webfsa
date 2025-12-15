617de9e5d6cd3061b0d2ea3f7102d5769d192a2f
hernanx32 <webfsa@gmail.com>
2025-12-15 20:09:04 -0300

    asdasdasdasd

---
+++ includes/pie.php
@@ -0,0 +1,122 @@
+<?PHP
+function pieindex($focus,$path)
+{
+?>
+<script src="<?PHP echo $path;?>comp/plugins/jquery/jquery.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/bootstrap/js/bootstrap.bundle.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables/jquery.dataTables.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-responsive/js/dataTables.responsive.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-responsive/js/responsive.bootstrap4.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/dataTables.buttons.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/buttons.bootstrap4.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/jszip/jszip.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/pdfmake/pdfmake.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/buttons.html5.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/buttons.print.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/buttons.colVis.js"></script>
+<script src="<?PHP echo $path;?>comp/dist/js/adminlte.min.js"></script>
+
+	
+</body>
+</html>
+<?php }
+
+
+function pieprincipal($focus,$path){
+?>
+<!-- jQuery -->    
+<script src="<?PHP echo $path;?>comp/plugins/jquery/jquery.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/bootstrap/js/bootstrap.bundle.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables/jquery.dataTables.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-responsive/js/dataTables.responsive.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-responsive/js/responsive.bootstrap4.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/dataTables.buttons.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/buttons.bootstrap4.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/jszip/jszip.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/pdfmake/pdfmake.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/buttons.html5.js"></script>
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/buttons.print.js"></script>
+
+<script src="<?PHP echo $path;?>comp/plugins/datatables-buttons/js/buttons.colVis.js"></script>
+
+<script src="<?PHP echo $path;?>comp/dist/js/adminlte.min.js"></script>
+
+<script>
+
+//SCRREM BAJAR CON ENTER
+document.addEventListener('keypress', function(evt) {
+  // Si el evento NO es una tecla Enter
+  if (evt.key !== 'Enter') {
+    return;
+  }
+  let element = evt.target;
+  // Si el evento NO fue lanzado por un elemento con class "focusNext"
+  if (!element.classList.contains('focusNext')) {
+    return;
+  }
+  // AQUI logica para encontrar el siguiente
+  let tabIndex = element.tabIndex + 1;
+  var next = document.querySelector('[tabindex="'+tabIndex+'"]');
+  // Si encontramos un elemento
+  if (next) {
+    next.focus();
+    event.preventDefault();
+  }
+});
+	
+	
+document.getElementById("<?php echo $focus;?>").focus();
+
+/*
+function bajarEnter (field, event) {
+	var keyCode = event.keyCode ? event.keyCode : event.which ? event.which : event.charCode;
+	if (keyCode == 13) {
+		var i;
+		for (i = 0; i < field.form.elements.length; i++)
+			if (field == field.form.elements[i])
+				break;
+		i = (i + 1) % field.form.elements.length;
+		field.form.elements[i].value=''; 
+		field.form.elements[i].focus();
+		return false;
+		} 
+		else
+	return true;
+	}*/
+</script>
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": false,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+	
+</script>
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Junco SAS.
+        </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2023-2024 <a href="https://stiformosa.ddns.net">Sistema de Gestion</a>.</strong> Todos los derechos reservados.
+  </footer>
+	
+</body>
+</html>
+<?PHP	}
--- index.php
+++ index.php
@@ -1,96 +1,39 @@
-<?php session_start();	?>
-<!doctype html>
-<html lang="es">
-<head>
+<?php
+session_start();
+$titulo='Sistema - Inicio';
+$path='';
 
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">	
-	<title>Sistema de Gestion</title>
-	
+include("Modulos/html.php");
+include("Modulos/conex.php");
 
-  	
-	<link rel="stylesheet" href="comp/all.min.css">
-	<link rel="stylesheet" href="comp/google.css">	
-	<link rel="stylesheet" href="comp/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="comp/adminlte.min.css">	
-	
-	
-    
+cabeza($titulo,$path);
 
-	
-</head>
-
-
-<?PHP
 if (isset($_GET['msj']))
 {
     $mensaje="<strong><span style='color: red;'>¡El Usuario y Clave son Incorrectos!..</span></strong>";    
    
 } else {
     $mensaje="<strong><span style='color: Green;'>Bienvenido..</span></strong>";  
-}	
-	
-	
-if (!isset($_GET['scr']))
-{
-  
- ?>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a class="h1"><b>Sistema de</b> Gestión</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg"><?php echo $mensaje; ?></p>
+}
 
-      <form id="form1" name="form1"  action="index.php?scr=ingresar" method="post">
-        <div class="input-group mb-3">
-          <input name="usuario" type="text" required="required" class="form-control" id="usuario" placeholder="Usuario" onkeypress="return bajarEnter(this, event)" size="20" maxlength="20">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input name="clave" type="password" required="required" class="form-control" id="clave" placeholder="Contraseña" size="20" maxlength="20">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-  <div class="row">
-          <!-- /.col -->
-    <div class="col-5">
-   	<!-- <a href="index.php?scr=olvidoclave">¿Olvido Su Clave?</a> -->
-   	</div>         
-    <div class="col-7">
-    <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
-    </div>
+if (!isset($_GET['scr'])){
+    
+    include("Modulos/Login/login.php");
+    $focus='usuario';
+}else{
+    $scr=$_GET['scr'];
 
-          <!-- /.col -->
-  </div>
-      </form>
- 
-   
-    </div>
-    <!-- /.card-body -->
-  </div>
-  <!-- /.card -->
-</div>
-	
-	
-		
-<?PHP }
-	else{
-		echo "Mostrando Datos";
-
+    if ($scr=="ingresar"){
+        include("Modulos/Login/ingresar.php");
+        $focus='usuario';   
     }
-	
+    if($scr=="olvidoclave"){
+    include("Modulos/Login/olvidoclave.php");
+    $focus='correo';
+    }
+}
+
+echo "Estado de Conexión: ".$EstCon ;
+pieindex($focus,$path);
+
 ?>
-		
-</body>
-</html>
