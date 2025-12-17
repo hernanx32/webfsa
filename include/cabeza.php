a9f24e9de5887991c748a6d8104ea6de3984c987
hernanx32 <webfsa@gmail.com>
2025-12-17 20:13:11 -0300

    12312323

--- include/cabeza.php
+++ include/cabeza.php
@@ -15,40 +15,28 @@ global $fecha_form;
 
 	
 	
-	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/google.css'>-->
-	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/adminlte.css'>
-	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/fontawesome-all.css'>
-	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/dataTables.bootstrap4.css'>
-	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/select2.css'>
+	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/google.css'>
+	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/adminlte.min.css'>
+	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/fontawesome-free-all.min.css'>
+	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/dataTables.bootstrap4.min.css'>
+	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/select2.min.css'>
 	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/buttons.bootstrap4.css'>
-
+	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/bootstrap.bundle.min.js.css'>
 	
 	
 	<script src='<?PHP echo $path;?>comp/js/adminlte.min.js'></script>
+	<script src="<?PHP echo $path;?>comp/js/jquery-3.6.0.min.js"></script>
+	<script src="<?PHP echo $path;?>comp/js/formularios.js"></script>
 	
-	
-	
-	
-	<!--
-	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/google.css'>
-  	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/plugins/fontawesome-free/css/all.min.css'>
-	<link rel='stylesheet' href='<?PHP echo $path;?>comp/css/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'>
-    <link rel='stylesheet' href='<?PHP echo $path;?>comp/css/adminlte.min.css'>	
-    <link rel='stylesheet' href='<?PHP echo $path;?>comp/plugins/select2/css/select2.min.css'>
-	<link rel='stylesheet' href='<?PHP echo $path;?>comp/plugins/datatables-buttons/css/buttons.bootstrap4.min.css'>
-	<script src='<?PHP echo $path;?>comp/js/formularios.js'></script>
-    <script src='<?PHP echo $path;?>comp/js/jquery-3.6.0.min.js'></script>
-    
-	<link href='<?PHP echo $path;?>comp/plugins/bootstrap/js/bootstrap.bundle.min.js.css' rel='stylesheet' type='text/css' media='screen'>
-		-->
+
   </head>
-	<!-- Preloader  
+	<!-- Preloader  -->
   <div class='preloader'>
    <a href='principal.php'> 
 	<img src='img/cargando.gif' alt='Cargando....' height='100' width='100'>
 	<img  src='img/cargando.png' alt='Cargando....' height='100' width='180'></a>
 	
-  </div> -->
+  </div> 
 <?PHP 
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 
